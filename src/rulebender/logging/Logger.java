f87957a5710e134bbda102d8ca663db78a98adf3
Adam <adammatthewsmith@users.noreply.github.com>
2013-05-19 03:36:18 +0000

    - New BNGParser. - Added apache lang lib to project and product dependencies.

--- src/rulebender/logging/Logger.java
+++ src/rulebender/logging/Logger.java
@@ -10,11 +10,20 @@ public class Logger
       SEVERE
     };
   
+    /**
+     * Just writing formatted logs to the console for now. Should update
+     * with writing to a log file later.   
+     * 
+     * @param level
+     * @param className
+     * @param message
+     */
   public static void log(LOG_LEVELS level, Class className, String message)
   {
     if (level == LOG_LEVELS.WARNING 
         || level == LOG_LEVELS.ERROR 
-        || level == LOG_LEVELS.SEVERE)
+        || level == LOG_LEVELS.SEVERE
+        )
     {
       String[] split = className.getName().split("\\.");
       
