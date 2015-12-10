b1113a39f852a315be2c18daa49fe32563065e84
Robert Clark <rhclark.pitt@gmail.com>
2015-12-10 15:50:26 -0500

    Add a comment about how two separate databases are kept in synch.

--- src/rulebender/preferences/PreferencesClerk.java
+++ src/rulebender/preferences/PreferencesClerk.java
@@ -27,6 +27,45 @@ import rulebender.preferences.PreferencesClerk;
 import rulebender.simulate.BioNetGenUtility;
 import rulebender.Activator;
 
+/*  This seems like a good time and place to document the datastructures that keep track of
+   the location of the workspace and the location of BioNetGen.  
+   
+   The location of the workspace is stored in the native java preference mechanism.  Look 
+   for the _preferences tag in the PickWorkspaceDialog().    You will see that certain fields
+   of this tag will store the location of the current workspace and you will also see that
+   a number of previously used workspaces is also stored in _preferences.
+   
+   For most purposeses, you can assume that the location of the workspace is stored in
+   _preferences  You can make changes to it, and I believe that at the end of the run,
+   the locations will be written to instanceLoc, to make the locations available for the next
+   run of RuleBender.  The existence of the second database, instanceLoc, creates a certain
+   amout of confusion,  but for most debugging tasks, you need only use  _preferences.
+   
+   The situation is somewhat more complicated with the location of BioNetGen.  This is 
+   because two very different dialog boxes are used to update the directory information.
+   The Preferences dialog box captures new settings that are typed by the user, and it
+   updates a variable that is pointed to by  SIM_PATH.  Access to that internal database is
+   provided only through the Preferences dialog box itself, and through methods.
+   
+   The decision was made to also permit the user to change the location of BioNetGen 
+   in the Workspace selection dialog box.  This dialog box works with ordinary scalar
+   variables, and there is now way to have it invoke a method when a variable needs to get
+   changed.
+   
+   So we have two dialog boxes that somehow need to modify the same data, but the two of
+   them have incompatible mechanisms for fetching and storing data.
+   
+   The resolution to this problem is to assume that the preference variable pointed to by
+   SIM_PATH always has the correct and up-to-date information.  If that information is 
+   needed to populate the PickWorkspace dialog box, then the scalars in the PickWorkspace
+   class are first updated by calling the methods of PreferenceClerk class to get 
+   current information.  After the PickWorkspace dialog box has been submitted by the
+   user, methods are then called to push that data back to the PreferencesClerk class.
+   This way the two databases are always kept in synch with one another.  It's crazy, but
+   it works.
+   
+*/
+
 public class PreferencesClerk 
 {
   // This is a more "native" type of preference store, and values seem to be stored immediately
