41b51755c2ca27471e9044a86b20940e019a6ad9
PrateekAdurty <51678093+PrateekAdurty@users.noreply.github.com>
2019-08-12 12:59:47 -0400

    Updating view object after rerun of FDLM
    uses the setCMAPNetworkViewer method in order to update the view object which holds the CMAP after rerunning the fdlm

--- src/rulebender/contactmap/prefuse/ContactMapVisual.java
+++ src/rulebender/contactmap/prefuse/ContactMapVisual.java
@@ -110,7 +110,11 @@ public class ContactMapVisual {
 
 		// Instantiate the NetworkViewer object.
 		m_networkViewer = new CMAPNetworkViewer(m_mainDisplaySize);
-
+		
+		//PA
+		//sets the view to the new updated CMAP after rerunning fdlm
+		view.setCMAPNetworkViewer(m_networkViewer);
+		
 		// Instantiate the Graph
 		/*
 		 * VisualItem.LABEL [Integer] | COMP_PARENT_LABEL ('molecule') [String] |
